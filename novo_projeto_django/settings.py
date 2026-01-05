2dc89df15d71b3191f102b61d7a992ff02fc761d
Luan Wittchind <102259505+luanwitch@users.noreply.github.com>
2026-01-05 10:54:58 -0300

    Atualizar o settings.py

--- novo_projeto_django/settings.py
+++ novo_projeto_django/settings.py
@@ -21,12 +21,12 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/5.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-m6j^f%p1z#1luj)_f8hnq*=*!rcff)xmt^%ii3j$81n@ub&n$6'
+SECRET_KEY = 'django-insecure-m6j^f%p1z#1luALLOWED_HOSTS = ['*']  j)_f8hnq*=*!rcff)xmt^%ii3j$81n@ub&n$6'
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = [*]
 
 
 # Application definition
